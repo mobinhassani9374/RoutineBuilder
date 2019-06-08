08e9d7762c6fba1fd5b25e1ebacbb899fbb964c4
mobinhassani <mobinhassani9374@gmail.com>
2019-06-08 15:17:04 +0430

    add Setting entity

---
+++ RoutineBuilder.Entities/Setting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace RoutineBuilder.Entities
+{
+    public class Setting : BaseEntity<int>
+    {
+        public SettingKey Key { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+
+    public enum SettingKey
+    {
+        [Display(Name = "نام کاربری برای لاگین کاربر")]
+        LoginUserName = 1,
+
+        [Display(Name = "رمزعبور برای لاگین کاربر")]
+        LoginPassword = 2
+    }
+}
