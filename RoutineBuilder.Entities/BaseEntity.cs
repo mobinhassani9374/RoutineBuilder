3c67c9dfc4d5b5cec5b2200a22b2f4ef75627fbe
mobinhassani <mobinhassani9374@gmail.com>
2019-06-08 15:11:48 +0430

    add BaseEntity

---
+++ RoutineBuilder.Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoutineBuilder.Entities
+{
+    public abstract class BaseEntity<TKey>
+    {
+        public TKey Id { get; set; }
+    }
+}
